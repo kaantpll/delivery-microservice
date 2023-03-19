4e668a532688b7560901f12c227284ca4f38c062
kaan topal <kaant43@gmail.com>
2023-03-19 13:55:02 +0300

    Product service creation

---
+++ product-service/src/main/java/com/example/productservice/dto/ProductDto.java
@@ -0,0 +1,15 @@
+package com.example.productservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    String name;
+    Long price;
+    Long stock;
+    String imgUrl;
+}
