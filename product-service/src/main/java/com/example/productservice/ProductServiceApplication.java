13487ea604c339fc624c9d1196c758ee8ceb6570
kaan topal <kaant43@gmail.com>
2023-03-19 13:43:23 +0300

    product entity creation

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -1,5 +1,6 @@
 package com.example.productservice;
 
+import com.example.productservice.entity.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
---
+++ product-service/src/main/java/com/example/productservice/entity/Product.java
@@ -0,0 +1,31 @@
+package com.example.productservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "stock")
+    private Long stock;
+
+    @Column(name = "price")
+    private Long price;
+
+    @Column(name = "img_url")
+    private String imgUrl;
+}
