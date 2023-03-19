de0d31506ee932f77c777594275102c0f2324dea
kaan topal <kaant43@gmail.com>
2023-03-19 13:32:57 +0300

    setting up product service infrasturcture.

--- product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/example/productservice/ProductServiceApplication.java
@@ -2,10 +2,10 @@ package com.example.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class ProductServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
