f034c9605d51ced8fbd430cd31621fc98be2496b
kaan topal <kaant43@gmail.com>
2023-03-24 21:34:38 +0300

    creation entity for order service

---
+++ order-service/src/main/java/com/example/orderservice/entity/Order.java
@@ -0,0 +1,37 @@
+package com.example.orderservice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.Instant;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @Column(name = "product_id")
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @Column(name = "product_id")
+    private String productId;
+    @Column(name = "quantity")
+    private long quantity;
+    @Column(name = "order_data")
+    private Instant orderData;
+    @Column(name = "amount")
+    private long amount;
+
+    private OrderStatus orderStatus;
+
+}
