dd2926db922d2a21182dd3096b1d4d4e7ed95fa0
kaan topal <kaant43@gmail.com>
2023-03-24 15:10:32 +0300

    improvement swagger response messages

--- product-service/src/main/java/com/example/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/example/productservice/controller/ProductController.java
@@ -5,7 +5,15 @@ import com.example.productservice.dto.request.CreateProductRequest;
 import com.example.productservice.dto.request.UpdateProductRequest;
 import com.example.productservice.dto.response.CreateProductResponse;
 import com.example.productservice.dto.response.UpdateProductResponse;
+import com.example.productservice.entity.Product;
 import com.example.productservice.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.models.media.MediaType;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +24,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/products")
+@Tag(name = "Product Service API v1", description = "Product Service ")
 public class ProductController {
 
     private final ProductService productService;
@@ -30,6 +39,25 @@ public class ProductController {
         return ResponseEntity.ok(productService.getAll());
     }
 
+    @Operation(
+            method = "GET",
+            summary = "get product with id",
+            description = "",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "",
+                            content = @Content(
+                                    array = @ArraySchema(schema = @Schema(implementation = Product.class))
+                            )
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "Product id is not exist",
+                            content = @Content(schema = @Schema(hidden = true))
+                    ),
+            }
+    )
     @GetMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ResponseEntity<ProductDto> getOne(@PathVariable String id){
