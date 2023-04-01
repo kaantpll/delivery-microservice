5a72dceec9ad4013ee0e66aaa0db2c51788b0710
kaan topal <kaant43@gmail.com>
2023-04-01 16:55:47 +0300

    creation common service for carrying event among micro services

---
+++ common-service/src/main/java/com/example/commonservice/dto/Order.java
@@ -0,0 +1,15 @@
+package com.example.commonservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+    private String productId;
+    private long quantity;
+}
