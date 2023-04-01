f2b71482bf529341a7377ad3731ad2b314bec792
kaan topal <kaant43@gmail.com>
2023-04-01 16:45:16 +0300

    add kafka configuration

---
+++ order-service/src/main/java/com/example/orderservice/producer/KafkaProducer.java
@@ -0,0 +1,27 @@
+package com.example.orderservice.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class KafkaProducer {
+
+    @Value("${spring.kafka.topic.name}")
+    private String topicName;
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage() {
+        //emit messsage
+    }
+}
